7136b9f9267565d0f0d111f35a332adf2676cf4c
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-06-15 17:38:18 -0300

    feat: cadastrop e login

--- app/dashboard/login/page.tsx
+++ app/dashboard/login/page.tsx
@@ -1,15 +1,41 @@
+'use client';
+
+import { Bruno_Ace } from 'next/font/google';
+import {useState} from 'react';
+
 export default function LoginPage(){
 
-    
+    const [loading, setLoading] = useState(false);
+    const [erro, setErro] = useState(false)
+
+    const handleLogin = async (cpf: string, senha: string) => {
+        setLoading(true)
+        try{
+            const r = await fetch('http://localhost:3001/api/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ cpf, senha })    
+            }
+            )
+
+            if(!r.ok){
+                setErro(true)
+                throw Error
+            }
+
+            console.log('Login bem sucedido')            
+
+        }catch(error){
+            console.log(erro)
+            setErro(true)
+        }finally{
+            setLoading(false)
+        }
+    }
 
     return(
-        <div>
-            <div>
-                <h1>Login</h1>
-                <p>Aqui você pode fazer login para acessar o dashboard.</p>
-            </div>
-            <div>
-            </div>
-        </div>
+         <button onClick={() => handleLogin('user@email.com', 'senha')}>
+      Fazer Login
+    </button>
     )
 }
--- app/landingPage/page.tsx
+++ app/landingPage/page.tsx
@@ -1,17 +1,21 @@
+
 export default function LandingPage(){
+
     return(
-        <header>
-            <div>
-                <img src=""></img>
-                <div className="p-2 gap-2">
-                    <button className="bg-[#052659] p-2 w-30">
-                        <a href="/dashboard/register" className="text-white">Criar Conta</a>
+        <div>
+            <div className="flex flex-row justify-between items-center p-4">
+                <div>
+                    <img src=""></img>
+                </div>
+                <div className="p-2 gap-2 ">
+                    <button className="bg-[#052659] p-2 w-40 mr-5 hover:bg-white transition all hover:border hover:border-[#052659]">
+                        <a href="/dashboard/register" className="text-white hover:text-[#052659]">Criar Conta</a>
                     </button>
-                    <button className="bg-white border border-[#052659] p-2 w-30 ">
-                        <a href="/dashboard/login" className="text-[#052659]">Entrar</a>
+                    <button className="bg-white border border-[#052659] p-2 w-40 hover:bg-[#052659] transition all">
+                        <a href="/dashboard/login" className="text-[#052659] hover:text-white">Entrar</a>
                     </button>
                 </div>
             </div>
-        </header>
+        </div>
     )
 }
